db82f29fe90a8ffc470438a398bae855592d6412
Muhammad Saddam Nur <Muhammad Saddam Nur@MIM-1609-118>
2017-02-24 06:35:04 +0700

    awal menggunakan codeigniter

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+include "phpqrcode/qrlib.php"; //<-- LOKASI FILE UTAMA PLUGINNYA
+ 
+$tempdir = "temp/"; //<-- Nama Folder file QR Code kita nantinya akan disimpan
+if (!file_exists($tempdir))#kalau folder belum ada, maka buat.
+    mkdir($tempdir);
+
+
+//lanjutan yang tadi
+ 
+#parameter inputan
+$isi_teks = "Belajar QR Code itu asik";
+$namafile = "coba.png";
+$quality = 'H'; //ada 4 pilihan, L (Low), M(Medium), Q(Good), H(High)
+$ukuran = 5; //batasan 1 paling kecil, 10 paling besar
+$padding = 0;
+ 
+QRCode::png("google.com");
+ 
+ 
+ 
+?>
